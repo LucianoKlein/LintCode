cdfcec1bef0c47a312d4b1b021f07b19bbd2fc3a
LucianoKlein <kleinersilver@gmail.com>
2017-08-29 23:11:45 +0800

    2017/8/29

--- LintCode/src/BasicLesson/ladder4_bfs/_598ZombieInMatrix/Solution.java
+++ LintCode/src/BasicLesson/ladder4_bfs/_598ZombieInMatrix/Solution.java
@@ -1,5 +1,11 @@
 package BasicLesson.ladder4_bfs._598ZombieInMatrix;
 
 public class Solution {
+	private final int ZOMBIE = 1;
+	private final int WALL = 2;
+	private final int PEOPLE = 0;
 	
+	public int zombie(int[][] grid) {
+		
+	}
 }
