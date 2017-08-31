20f77315423adb5609f038658d2be6873071c5f1
LucianoKlein <kleinersilver@gmail.com>
2017-08-31 14:16:03 +0800

    solve MapReduce easy

--- LintCode/src/easyLintcode/_499WordCountMapReduce/Solution.java
+++ LintCode/src/easyLintcode/_499WordCountMapReduce/Solution.java
@@ -1,5 +1,42 @@
 package easyLintcode._499WordCountMapReduce;
 
-public class Solution {
+import java.util.StringTokenizer;
 
+/**
+ * Definition of OutputCollector:
+ * class OutputCollector<K, V> {
+ *     public void collect(K key, V value);
+ *         // Adds a key/value pair to the output buffer
+ * }
+ */
+public class WordCount {
+
+	public static class Map {
+        public void map(String key, String value, OutputCollector<String, Integer> output) {
+            // Write your code here
+            // Output the results into output buffer.
+            // Ps. output.collect(String key, int value);
+        	//把value字符分割?
+            StringTokenizer tokenizer = new StringTokenizer(value);
+            while (tokenizer.hasMoreTokens()) {
+                String word = tokenizer.nextToken();
+                output.collect(word, 1);
+            }
+        }
+    }
+
+    public static class Reduce {
+        public void reduce(String key, Iterator<Integer> values,
+                           OutputCollector<String, Integer> output) {
+            // Write your code here
+            // Output the results into output buffer.
+            // Ps. output.collect(String key, int value);
+            int sum = 0;
+            while (values.hasNext()) {
+                    sum += values.next();
+            }
+            output.collect(key, sum);
+        }
+    }
 }
+
